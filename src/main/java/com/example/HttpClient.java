2e271259ba8aa0365002565341208b40a77bbd8f
elianrenteria <elianrenteriadevelopment@gmail.com>
2024-09-21 13:48:56 -0700

    added post function

--- src/main/java/com/example/HttpClient.java
+++ src/main/java/com/example/HttpClient.java
@@ -1,16 +1,35 @@
 package com.example;
-import java.io.IOException;
 
+import java.io.IOException;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
 public class HttpClient {
+    
     public static void get(String url) {
         try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
             HttpGet request = new HttpGet(url);
+            HttpResponse response = httpClient.execute(request);
+            String responseBody = EntityUtils.toString(response.getEntity());
+
+            System.out.println("Response Code: " + response.getStatusLine().getStatusCode());
+            System.out.println("Response Body: " + responseBody);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void post(String url, String jsonPayload) {
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpPost request = new HttpPost(url);
+            request.setHeader("Content-Type", "application/json");
+            request.setEntity(new StringEntity(jsonPayload));
+
             HttpResponse response = httpClient.execute(request);
             String responseBody = EntityUtils.toString(response.getEntity());
 
